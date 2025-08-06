495152879124d7ad7e8713549bc40f44d804783f
Shuflduf <shuflduf@gmail.com>
2025-08-06 03:51:58 -0600

    quick drop

--- Player/vertical_movement.gd
+++ Player/vertical_movement.gd
@@ -6,11 +6,14 @@ const HOP_FORCE: float = -400.0
 const HOP_WINDOW: float = 0.1
 const MAX_COYOTE_TIME: float = 0.1
 const GRAVITY_MULT: float = 2.0
+const QUICK_DROP_TIME: float = 0.2
 
 var jumping: bool = false
 var jump_timer: float = 0.0
 var has_double_jump: bool = false
 var coyote_timer: float = 0.0
+var quick_drop_timer: float = 0.0
+var drop_queued: bool = false
 
 func _physics_process(delta: float) -> void:
     p.velocity.y += p.get_gravity().y * delta * GRAVITY_MULT
@@ -19,6 +22,7 @@ func _physics_process(delta: float) -> void:
     if p.is_on_floor():
         coyote_timer = 0.0
         has_double_jump = true
+        quick_drop_timer = 0.0
     else:
         coyote_timer += delta
 
@@ -39,6 +43,16 @@ func _physics_process(delta: float) -> void:
             if jump_timer > HOP_WINDOW:
                 apply_jump_vel(JUMP_FORCE)
 
+    if Input.is_action_just_released(&"down") and quick_drop_timer == 0.0:
+        quick_drop_timer += delta
+
+    if quick_drop_timer != 0.0:
+        quick_drop_timer += delta
+        if Input.is_action_just_pressed(&"down"):
+            p.velocity.y = MAX_FALL
+
+    if quick_drop_timer > QUICK_DROP_TIME:
+        quick_drop_timer = 0.0
 
 func apply_jump_vel(strength: float) -> void:
     p.velocity.y = strength
