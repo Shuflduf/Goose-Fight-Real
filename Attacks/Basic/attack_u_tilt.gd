b79e0bc95b14a57ebf09cecc2d61fc3a4e1494c5
Shuflduf <shuflduf@gmail.com>
2025-08-09 14:44:59 -0600

    autilt actually DIDNT work first try

--- Attacks/Basic/attack_u_tilt.gd
+++ Attacks/Basic/attack_u_tilt.gd
@@ -1,4 +1,15 @@
 extends BoringAttack
 
+var touched_ground: bool = true
+
 func jump() -> void:
-    p.velocity.y = -500.0
+
+    if touched_ground:
+        touched_ground = false
+        p.velocity.y = -500.0
+    else:
+        p.velocity.y = -50.0
+
+func _physics_process(_delta: float) -> void:
+    if p.is_on_floor():
+        touched_ground = true
