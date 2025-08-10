c6d2ebcf6f624cbb740520a4b6dd92b45c4c87e4
Shuflduf <shuflduf@gmail.com>
2025-08-10 11:00:48 -0600

    abspec, now with more pepsi

---
+++ Attacks/Special/Aerial/attack_ab_spec.gd
@@ -0,0 +1,19 @@
+extends BoringAttack
+
+func start_particles() -> void:
+    %Bottle.show()
+    %Particles.emitting = true
+
+func stop_particles() -> void:
+    %Bottle.hide()
+    %Particles.emitting = false
+    p.velocity.x = -700.0 * dir_mult()
+
+
+func _physics_process(delta: float) -> void:
+    if %Particles.emitting:
+        p.velocity.x = lerp(p.velocity.x, -500.0 * dir_mult(), delta * 10.0)
+        p.velocity.y = -100.0
+
+func flip() -> void:
+    $Visuals.scale.x = dir_mult()
