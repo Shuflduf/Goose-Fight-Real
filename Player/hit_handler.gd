8dbefa4cb8d05fd22ad669bc9aaa61be7c05e619
Shuflduf <shuflduf@gmail.com>
2025-08-12 05:16:59 -0600

    much better stun. still doesnt do shit

--- Player/hit_handler.gd
+++ Player/hit_handler.gd
@@ -6,5 +6,28 @@ func process_hit(data: DamageData) -> void:
     p.velocity = data.knockback * (0.005 * p.health + 1)
     p.health += data.health
     p.health_indicator.value = p.health
+    stun()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+    if p.inp.event_is_action_just_pressed(event, &"debug"):
+        stun()
+
+
+func stun() -> void:
     p.state = p.MoveState.Stunned
-    %Sprites.modulate = Color(0.7, 0.7, 0.7, 1.0)
+    %Sprites.modulate = Color.DIM_GRAY
+    $StunTimer.start()
+
+
+func _on_stun_timer_timeout() -> void:
+    p.state = p.MoveState.Both
+    %Sprites.modulate = Color.WHITE
+
+
+func _physics_process(_delta: float) -> void:
+    if p.is_on_floor() and p.state == p.MoveState.Stunned:
+        $StunTimer.stop()
+        # this MIGHT cause issues MAYBE
+        await get_tree().create_timer(0.1).timeout
+        _on_stun_timer_timeout()
